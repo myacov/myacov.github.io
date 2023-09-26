41abd09a81d836a7bdc6b026f5c9f7d7b55f17e4
Yacov Medina <m.yacov@gmail.com>
2023-09-26 14:25:23 +0300

    revamp project section

---
+++ assets/js/loadProjects.js
@@ -0,0 +1,37 @@
+// Fetch the projects.json file
+fetch('./projects.json')
+    .then(response => response.json())
+    .then(projects => {
+        // Get the projects container
+        const container = document.getElementById('projects-container');
+      
+        // Iterate through the projects array and create the HTML elements
+        projects.forEach(project => {
+            // Check if the project should be displayed
+            if (project.display) {  
+              // Create an article for each project
+              const article = document.createElement('article');
+          
+              // Add the project details
+              article.innerHTML = `
+                  <header>
+                    <h2>${project.title}</h2>
+                  </header>
+                  <a href="${project.link}" class="image fit" target="_blank" rel="noopener noreferrer">
+                    <img src="${project.image}" alt="${project.title}" />
+                  </a>
+                  <p>${project.description}</p>
+                  <p>Technologies Used: ${project.technologies.join(', ')}</p>
+                  <ul class="actions special">
+                    <li><a href="${project.link}" class="button" target="_blank" rel="noopener noreferrer">Check it out</a></li>
+                  </ul>
+              `;
+          
+              // Append the article to the container
+              container.appendChild(article);
+            }
+        });
+    })
+    .catch(error => {
+        console.error("Error loading projects:", error);
+    });
